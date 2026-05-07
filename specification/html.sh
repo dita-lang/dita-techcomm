daeb7016b0bb30018f0d5915d123030786600159
Robert D. Anderson <robert.dan.anderson@oracle.com>
2026-05-06 22:48:08 -0500

    Fix bad xmlelement markup

---
+++ specification/html.sh
@@ -0,0 +1,2 @@
+~/dita-ot-4.4/bin/dita install
+~/dita-ot-4.4/bin/dita -i dita-2.0-technical-content-specification.ditamap -f spec-html5 -o out --filter=resources/DITA2.0-tc-spec.ditaval -t ../temp -Dclean.temp=no 
